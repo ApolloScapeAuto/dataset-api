bbf4c5dadda527d4c0a3e34a9b4bf12423d54bd3
wangpeng54 <wangpeng54@baidu.com>
2018-07-02 15:00:15 -0700

    add sub module

--- lane_segmentation/install.sh
+++ lane_segmentation/install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # make sure pip and setuptools are available on your system
 
-pip install -r requirements.txt
 curr_dir=`pwd`
-<<<<<<< 49d5b0ccba1081bb6fe0abb57fb6c311e165dbe7
-
-=======
->>>>>>> updated version zdf
 cd thirdParty/cityscapesScripts/
 if [ -f "__init__.py" ]; then
     rm __init__.py
