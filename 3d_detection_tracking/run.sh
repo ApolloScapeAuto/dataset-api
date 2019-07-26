8c9bba2c6cc7d6681ea446e1ce1b3afba7b87d86
Sibo Zhang <sibozhang@users.noreply.github.com>
2019-07-25 19:13:39 -0700

    rm cuda visible devices

--- 3d_detection_tracking/run.sh
+++ 3d_detection_tracking/run.sh
@@ -3,8 +3,6 @@
 source activate apolloscape
 
 # export NUMBA_ENABLE_CUDASIM=1
-# export CUDA_VISIBLE_DEVICES=4
-export CUDA_VISIBLE_DEVICES=4,5,6,7
 
 # tracking
 python eval.py --modeType=tracking --gtPath=../track/apollo_lab --dtPath=../track/apollo_res --typeFilterFlag
