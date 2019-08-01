6a0ac4b8eef626a48a1d3467bf4651033798a048
Sibo Zhang <sibozhang@users.noreply.github.com>
2019-07-31 22:32:36 -0700

    Update run.sh

--- 3d_detection_tracking/run.sh
+++ 3d_detection_tracking/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# export NUMBA_ENABLE_CUDASIM=1
+# detection
+python eval.py --gtPath=apollo_lab_test --dtPath=apollo_res_test --apSampleNum=10 #--typeFilterFlag #2>&1 | tee run.log
 
 # tracking
-python eval.py --modeType=tracking --gtPath=../track/apollo_lab --dtPath=../track/apollo_res --typeFilterFlag
-# detection
-# python eval.py --gtPath=apollo_lab_test --dtPath=apollo_res_test --apSampleNum=10 #--typeFilterFlag #2>&1 | tee run.log
+# python eval.py --modeType=tracking --gtPath=../track/apollo_lab --dtPath=../track/apollo_res --typeFilterFlag
+
